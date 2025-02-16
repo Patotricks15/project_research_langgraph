051deef0d627b4231bed884b56d0ae3f5536a8b6
Patotricks <patrickufrrj@gmail.com>
2025-02-16 14:52:35 -0300

    Writing docstrings

--- services/db_service.py
+++ services/db_service.py
@@ -4,19 +4,45 @@ import os
 selected_db_service = None
 
 def set_db_service(service):
-    
+    """
+    Sets the database service to be used globally.
+
+    Args:
+        service (DBService): The database service to be used.
+    """
     global selected_db_service
     selected_db_service = service
 
 
 class MongoDBService:
     def __init__(self, database_name: str, collection_name: str):
+        """
+        Initialize a MongoDBService instance.
+
+        Args:
+            database_name (str): The name of the MongoDB database to connect to.
+            collection_name (str): The name of the MongoDB collection to connect to.
+
+        Attributes:
+            client (MongoClient): The MongoClient instance used to connect to the MongoDB database.
+            db (Database): The MongoDB database instance.
+            collection (Collection): The MongoDB collection instance.
+        """
         mongo_uri = os.getenv("MONGO_URI", "mongodb://mongo:27017/")
         self.client = MongoClient(mongo_uri)
         self.db = self.client[database_name]
         self.collection = self.db[collection_name]
 
     def insert_document(self, document_dict):
+        """
+        Inserts a document into the MongoDB collection.
+
+        Args:
+            document_dict (dict): A dictionary with the fields of the document to be inserted.
+
+        Returns:
+            str: The _id of the inserted document.
+        """
         result_insert = self.collection.insert_one(document_dict)
         print("Documento inserido com _id:", result_insert.inserted_id)
         return result_insert.inserted_id
--- services/llm_service.py
+++ services/llm_service.py
@@ -8,16 +8,47 @@ from services import db_service
 
 
 def search_arxiv(state):
+    """
+    Performs a search on arXiv and retrieves search results.
+
+    Args:
+        state (dict): A dictionary containing the key 'question' with a list 
+                      where the first element is the search query.
+
+    Returns:
+        dict: A dictionary containing the key 'context' with the retrieved documents.
+    """
+
     retriever = ArxivRetriever(load_max_docs=2, get_ful_documents=True)
     docs = retriever.invoke(state['question'][0])
     result = {"context": [docs]}
     return result
 
 def read_question(state):
+    """
+    Reads the question from the state and puts it into the result.
+    
+    Args:
+        state (dict): A dictionary containing the key 'question' with a list 
+                      where the first element is the question to be answered.
+
+    Returns:
+        dict: A dictionary containing the key 'question' with the question to be answered.
+    """
     result = {"question": state['question']}
     return state
 
 def search_tavily(state):
+    """
+    Performs a search using Tavily and retrieves search results.
+
+    Args:
+        state (dict): A dictionary containing the key 'question' with a list 
+                      where the first element is the search query.
+
+    Returns:
+        dict: A dictionary containing the key 'context' with the search results.
+    """
     tavily_search = TavilySearchResults(max_results=3)
     search_docs = tavily_search.invoke(state['question'][0])
     formatted_search_docs = "\n\n---\n\n".join(
@@ -27,6 +58,18 @@ def search_tavily(state):
     return result
 
 def search_wikipedia(state):
+    """
+    Performs a search using Wikipedia and retrieves search results.
+
+    Args:
+        state (dict): A dictionary containing the key 'question' with a list 
+                      where the first element is the search query.
+
+    Returns:
+        dict: A dictionary containing the key 'context' with a list of two strings, 
+              where each string contains the content of each search result.
+    """
+    
     search_docs = WikipediaLoader(query=state['question'][0], load_max_docs=2).load()
     formatted_search_docs = "\n\n---\n\n".join(
         [f'<Document source="{doc.metadata["source"]}" page="{doc.metadata.get("page", "")}">\n{doc.page_content}\n</Document>' for doc in search_docs]
@@ -35,12 +78,34 @@ def search_wikipedia(state):
     return result
 
 def search_duck_duck_go(state):
+    """
+    Performs a search using DuckDuckGo and retrieves search results.
+
+    Args:
+        state (dict): A dictionary containing the key 'question' with a list 
+                      where the first element is the search query.
+
+    Returns:
+        dict: A dictionary containing the key 'context' with the search results.
+    """
+
     search = DuckDuckGoSearchRun()
     search_docs = search.invoke(state['question'][0])
     result = {"context": [search_docs]}
     return result
 
 def generate_summary(state):
+    """
+    Generates a summary of the given question using the context provided.
+
+    Args:
+        state (dict): A dictionary containing the following keys:
+            - 'context' (str or list): The context to use when generating the summary.
+            - 'question' (list): A list of strings where the first element is the question to be answered.
+
+    Returns:
+        dict: A dictionary containing the key 'pre_answers' with the generated summary as its value.
+    """
     llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
     context = state['context']
     question = state['question'][0]
@@ -54,6 +119,18 @@ def generate_summary(state):
     return result
 
 def final_answer(state):
+    """
+    Generates a final and unified answer to a given question using pre-existing answers.
+
+    Args:
+        state (dict): A dictionary containing the following keys:
+            - 'pre_answers' (str): Pre-existing answers that will be used to form the final answer.
+            - 'question' (list): A list where the first element is the question that needs to be answered.
+
+    Returns:
+        dict: A dictionary containing the key 'final_output' with the generated final answer as its value.
+    """
+
     llm = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
     pre_answers = state['pre_answers']
     question = state['question'][0]
